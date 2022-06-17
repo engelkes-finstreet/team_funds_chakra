6b95cb1e59920ebaae307a620b15e0d2b9eda53e
Patrick Engelkes <patrick.engelkes@finstreet.de>
2022-06-17 23:36:19 +0200

    Add initial migration and update build command to migrate production database.

---
+++ prisma/migrations/20220617213406_initial_migration/migration.sql
@@ -0,0 +1,236 @@
+-- CreateEnum
+CREATE TYPE "Position" AS ENUM ('PLAYER', 'COACH', 'ASSISTANT');
+
+-- CreateEnum
+CREATE TYPE "PunishmentType" AS ENUM ('MONEY', 'BEER');
+
+-- CreateEnum
+CREATE TYPE "PaymentType" AS ENUM ('ADMIN', 'PAYPAL');
+
+-- CreateTable
+CREATE TABLE "AdminUser" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "isApproved" BOOLEAN NOT NULL DEFAULT false,
+    "isConfirmed" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "AdminUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "isConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ConfirmUserToken" (
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ResetUserPasswordToken" (
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ConfirmAdminUserToken" (
+    "adminId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "ResetAdminUserPasswordToken" (
+    "adminId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Season" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "adminUserId" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Season_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "position" "Position" NOT NULL DEFAULT E'PLAYER',
+    "slug" TEXT NOT NULL,
+    "adminUserId" TEXT NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Punishment" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "type" "PunishmentType" NOT NULL,
+    "adminUserId" TEXT NOT NULL,
+    "seasonId" TEXT NOT NULL,
+
+    CONSTRAINT "Punishment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlayerPayments" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "adminUserId" TEXT,
+    "paymentType" "PaymentType" NOT NULL,
+    "playerId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "type" "PunishmentType" NOT NULL,
+    "seasonId" TEXT NOT NULL,
+
+    CONSTRAINT "PlayerPayments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlayerPunishments" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "adminUserId" TEXT NOT NULL,
+    "playerId" TEXT NOT NULL,
+    "punishmentId" TEXT NOT NULL,
+    "seasonId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+
+    CONSTRAINT "PlayerPunishments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminUser_email_key" ON "AdminUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminUser_slug_key" ON "AdminUser"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_slug_key" ON "User"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ConfirmUserToken_userId_key" ON "ConfirmUserToken"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ConfirmUserToken_token_key" ON "ConfirmUserToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ResetUserPasswordToken_userId_key" ON "ResetUserPasswordToken"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ResetUserPasswordToken_token_key" ON "ResetUserPasswordToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ConfirmAdminUserToken_adminId_key" ON "ConfirmAdminUserToken"("adminId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ConfirmAdminUserToken_token_key" ON "ConfirmAdminUserToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ResetAdminUserPasswordToken_adminId_key" ON "ResetAdminUserPasswordToken"("adminId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ResetAdminUserPasswordToken_token_key" ON "ResetAdminUserPasswordToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Season_slug_key" ON "Season"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_slug_key" ON "Player"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Player_userId_key" ON "Player"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Punishment_name_key" ON "Punishment"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Punishment_slug_key" ON "Punishment"("slug");
+
+-- AddForeignKey
+ALTER TABLE "ConfirmUserToken" ADD CONSTRAINT "ConfirmUserToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResetUserPasswordToken" ADD CONSTRAINT "ResetUserPasswordToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ConfirmAdminUserToken" ADD CONSTRAINT "ConfirmAdminUserToken_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResetAdminUserPasswordToken" ADD CONSTRAINT "ResetAdminUserPasswordToken_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Season" ADD CONSTRAINT "Season_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Player" ADD CONSTRAINT "Player_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Player" ADD CONSTRAINT "Player_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Punishment" ADD CONSTRAINT "Punishment_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Punishment" ADD CONSTRAINT "Punishment_seasonId_fkey" FOREIGN KEY ("seasonId") REFERENCES "Season"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPayments" ADD CONSTRAINT "PlayerPayments_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "AdminUser"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPayments" ADD CONSTRAINT "PlayerPayments_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPayments" ADD CONSTRAINT "PlayerPayments_seasonId_fkey" FOREIGN KEY ("seasonId") REFERENCES "Season"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPunishments" ADD CONSTRAINT "PlayerPunishments_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "AdminUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPunishments" ADD CONSTRAINT "PlayerPunishments_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPunishments" ADD CONSTRAINT "PlayerPunishments_punishmentId_fkey" FOREIGN KEY ("punishmentId") REFERENCES "Punishment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerPunishments" ADD CONSTRAINT "PlayerPunishments_seasonId_fkey" FOREIGN KEY ("seasonId") REFERENCES "Season"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
